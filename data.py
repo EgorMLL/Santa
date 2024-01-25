0e8ca9ccfc24705b6f6e140b40d861c2968d5506
EgorMLL <ymlechkov@mail.ru>
2024-01-25 13:38:05 +0300

    Добавил основные файлы в проект.

---
+++ data.py
@@ -0,0 +1,24 @@
+import json
+
+# todo: написать функцию load_user_data
+def load_user_data(data_path):
+    """Функция, которая загружает данные пользователей из файла и возвращает словарь.
+    Если файла нет, то возвращается пустой словарь.
+    :param data_path: путь к файлу с данными
+    :return: словарь с данными пользователей
+    """
+    try:
+        with open(data_path, "r") as file:
+            json.load(file)
+    except:
+        return {}
+
+
+# todo: написать функцию save_user_data
+def save_user_data(user_data, data_path):
+    """Функция, которая сохраняет данные пользователей в файл.
+    :param user_data: словарь с данными пользователей
+    :param data_path: путь к файлу с данными
+    """
+    with open(data_path, "w") as file:
+        json.dump(user_data, file)
